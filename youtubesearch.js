157945caef52f8e582ebb27b0cba03208a9be342
Nathen Watters <wattersnathen@gmail.com>
2015-02-28 17:17:58 -0700

    updated search project to include infinite scroll ability

--- youtubesearch.js
+++ youtubesearch.js
@@ -1,6 +1,8 @@
 "use strict"
 $(document).ready(function() {
 
+  var isWorking = 0;
+
   //_________________________show loading text when ajax starts
   $(document).bind('ajaxStart', function() {
     $("input[type='submit']").attr('value', '...loading');
@@ -10,13 +12,26 @@ $(document).ready(function() {
     $("input[type='submit']").attr('value', 'Submit');
   });
 
+  //_________________________
+  $(window).on('scroll', scrollDebounce(function(event) {
+    var wintop = $(window).scrollTop(), 
+        docHeight = $(document).height(),
+        winHeight = $(window).height();
+    var scrollTrigger = 0.90;
+
+    if ((wintop / (docHeight - winHeight)) > scrollTrigger) {
+      nextPage();
+    } 
+      
+  }, 250));
+
   //_________________________initiate app on form submit event
   $(function() {
     $("#search-youtube").submit(function( event ) {
       event.preventDefault();   // don't refresh the page
 
       // push the form area to the top of the page
-      $("form").removeClass("start-middle").addClass("go-high");
+      $("div.start-middle").removeClass("start-middle").addClass("go-high");
 
       var searchTerm = $("#search-query").val();
       getResults(searchTerm);
@@ -25,28 +40,71 @@ $(document).ready(function() {
 
   //_________________________make the $.getJSON call
   function getResults( searchTerm ) {
+    var url = 'https://www.googleapis.com/youtube/v3/search'; // endpoint url
     var params = {
-      part: 'snippet',          // required for YouTube v3 API request
+      part: 'snippet, id',          // required for YouTube v3 API request
       key: 'AIzaSyBIVxF2SP7ozlaVsOfTB8nj-1TJhkP3NsI',     // developer key
-      type: 'video',            // only want videos, not playlists or channels
+      type: 'video',
       videoEmbeddable: 'true',  // only return videos that can be embedded
-      maxResults: 10,           // limit the search results to max of 10
+      maxResults: 5,           // limit the search results to max of 10
       q: searchTerm             // query parameter
-    },
-    url = 'https://www.googleapis.com/youtube/v3/search'; // endpoint url
+    };
 
-    $.getJSON(url, params, function ( data ) {
-      showResults(data.items);
-    });
-  }
-
-  //_________________________add the results to the DOM
-  function showResults( results ) {
-    var html = '';
-    $.each(results, function( index, value ) {
-      html += "<iframe src='http://www.youtube.com/embed/" + value.id.videoId + "' frameborder='0' allowfullscreen></iframe><br>";
-    });
-    $("#search-results").detach().html(html).appendTo(".container");
+    makeAjaxCall(url, params);
+    
   }
 
 });
+
+function scrollDebounce(func, interval) {
+  var lastcall = -1;
+  return function() {
+    clearTimeout(lastcall);
+    var args = arguments;
+    var self = this;
+    lastcall = setTimeout(function() {
+      func.apply(self, args);
+    }, interval);
+  };
+}
+
+function nextPage() {
+
+  var token = $('#search-results').data('token');
+  var query = $('#search-results').data('query');
+  var dUrl  = $('#search-results').data('url');
+
+  var parameters = {
+    part: 'snippet, id',          // required for YouTube v3 API request
+    key: 'AIzaSyBIVxF2SP7ozlaVsOfTB8nj-1TJhkP3NsI',     // developer key
+    type: 'video',
+    pageToken: token,
+    videoEmbeddable: 'true',  // only return videos that can be embedded
+    maxResults: 5,           // limit the search results to max of 10
+    q: query             // query parameter
+  };
+
+  makeAjaxCall(dUrl, parameters);
+}
+
+function makeAjaxCall(url, params) {
+  var nextPageToken;
+  $.getJSON(url, params, function ( data ) {
+    $("#search-results").data("token", data.nextPageToken);
+    $("#search-results").data("query", params.q);
+    $("#search-results").data("url", url);
+    showResults(data.items);
+  }).fail(function(jqxhr, textStatus, error){
+    console.log("Error returned: " + textStatus + ", " + error);
+  });
+}
+
+//_________________________add the results to the DOM
+function showResults( results ) {
+
+  var html = '';
+  $.each(results, function( index, value ) {
+    html += "<iframe src='http://www.youtube.com/embed/" + value.id.videoId + "' frameborder='0' allowfullscreen></iframe><br>";
+  });
+  $("#search-results").append(html);
+}
